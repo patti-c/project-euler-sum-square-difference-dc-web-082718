98a4c28d9177cb50153626f2803a11a8f68b8767
patti-c <patricia.carter@flatironschool.com>
2018-11-27 09:20:28 -0500

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+
+  def initialize(num)
+    @num = num
+  end
+
+  def square_of_sum
+    (1..@num).reduce(:+) * (1..@num).reduce(:+)
+  end
+
+  def sum_of_square
+    (1..@num).map{|x| x * x}.reduce(:+)
+  end
+
+  def difference
+    square_of_sum - sum_of_square
+  end
+
+end
+
+puts SumSquareDifference.new(5).difference
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+
+def square_of_sum (num)
+  (1..num).reduce(:+) * (1..num).reduce(:+)
+end
+
+def sum_of_square(num)
+  (1..num).map{|x| x * x}.reduce(:+)
+end
+
+def sum_square_difference(num)
+  square_of_sum(num) - sum_of_square(num)
+end
+
+puts sum_square_difference(10)
